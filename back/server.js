453e79e82ff569801d1959390b5b73a4c9565faf
Jack Hickman <up810066@myport.ac.uk>
2018-03-10 17:15:03 +0000

    Got server working

---
+++ back/server.js
@@ -0,0 +1,14 @@
+'use static';
+
+const express = require('express')
+const app = express()
+const path = require('path')
+//const db = require(sql-model);
+
+app.listen(8080, (err) => {
+    if (err) console.error('error starting server', err);
+    else console.log('server started');
+});
+
+app.use('/', express.static(path.join(__dirname, '../front/'), { extensions: ['html'] }));
+//app.get("/login", express.static());
--- back/sql-model.js
+++ back/sql-model.js
@@ -3,3 +3,47 @@
 const mysql = require('mysql2/promise');
 
 const config = require('./config.json');
+
+// create one connection to the database
+let sqlPromise = null;
+
+async function init() {
+  if (sqlPromise) return sqlPromise;
+
+  sqlPromise = newConnection();
+  return sqlPromise;
+}
+
+async function shutDown() {
+  if (!sqlPromise) return;
+  const stashed = sqlPromise;
+  sqlPromise = null;
+  await releaseConnection(await stashed);
+}
+
+async function newConnection() {
+  // todo: this should really use connection pools
+  const sql = await mysql.createConnection(config.mysql);
+
+  // handle unexpected errors by just logging them
+  sql.on('error', (err) => {
+    console.error(err);
+    sql.end();
+  });
+
+  return sql;
+}
+
+async function releaseConnection(connection) {
+  await connection.end();
+}
+
+
+// for debugging, we should log unhandled promise rejections
+process.on('unhandledRejection', console.error);
+
+
+/* eslint-disable object-shorthand */
+module.exports = {
+  shutDown: shutDown
+};
